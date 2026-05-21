9d7c70547f388f1070a3a6bd6182c886e72ed0db
lovely-sjmedia <lovely@sjmedialabs.com>
2026-05-21 17:08:14 +0530

    fixed margin

--- src/components/blocks/ErpHero.tsx
+++ src/components/blocks/ErpHero.tsx
@@ -44,7 +44,7 @@ export function ErpHero({ content }: ErpHeroProps) {
   };
 
   return (
-    <section className="relative w-full py-16 mt-10 bg-[#fdfeff] overflow-hidden">
+    <section className="relative w-full py-16 bg-[#fdfeff] overflow-hidden">
       {/* Background Grid Pattern */}
       {/* <div className="absolute inset-0 bg-[linear-gradient(to_right,#e2e8f0_1px,transparent_1px),linear-gradient(to_bottom,#e2e8f0_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)] opacity-60 -z-10" /> */}
 
